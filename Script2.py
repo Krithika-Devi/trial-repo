4c19233b42b0b710ac0cf7e95b36947c787df7d9
Krithika Devi Chandran <65224859+Krithika-Devi@users.noreply.github.com>
2022-08-17 12:32:03 +0530

    Update Script2.py

--- Script2.py
+++ Script2.py
@@ -1,4 +1,8 @@
-if n1<n2:
-  print("n1 is smaller")
-else:
-  print("n2 is smaller")
+%%writefile Script2.py
+def smaller():
+  n1 = int(input("Enter number, n1: "))
+  n2 = int(input("Enter number, n2: "))
+  if n1<n2:
+    print("n1 is smaller")
+  else:
+    print("n2 is smaller")
