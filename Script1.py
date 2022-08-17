0c9db811c1fda8bb7695ec913c4888ebcc665d63
Krithika Devi Chandran <65224859+Krithika-Devi@users.noreply.github.com>
2022-08-17 12:31:40 +0530

    Update Script1.py

--- Script1.py
+++ Script1.py
@@ -1,4 +1,8 @@
-if n1>n2:
-  print("n1 is greater")
-else:
-  print("n2 is greater")
+%%writefile Script1.py
+def greater():
+  n1 = int(input("Enter number, n1: "))
+  n2 = int(input("Enter number, n2: "))
+  if n1>n2:
+    print("n1 is greater")
+  else:
+    print("n2 is greater")
